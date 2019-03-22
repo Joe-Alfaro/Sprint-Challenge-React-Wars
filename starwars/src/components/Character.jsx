beaada1271455793c4cc64d07f987d704d4435ec
joe-alfaro <josephlalfaro@gmail.com>
2019-03-22 09:54:10 -0700

    display character data and styled on to cards

--- starwars/src/components/Character.jsx
+++ starwars/src/components/Character.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
+import './StarWars.css';
+
 const Character = props => {
   return (
-    <h2>{props.starwarsChar.name}</h2> 
+    <div className='character-card'>
+      <h2>{props.starwarsChar.name}</h2>
+      <p>Born: {props.starwarsChar.birth_year}</p>
+      <p>Gender: {props.starwarsChar.gender}</p>
+      <p>Height: {props.starwarsChar.height}cm</p>
+      <p>Weight: {props.starwarsChar.mass}kg</p>
+    </div>
   );
 }
 
